058341f5a19780ad0dd0687c5d046e05e19a9bd4
Soohwan Kim <sh951011@gmail.com>
2021-09-08 02:35:27 +0900

    Create koelectra_demo.py

---
+++ koelectra_demo.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModel, pipeline
+
+nlp = pipeline("fill-mask")
+
+st.title('Fill Mask')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
+model = AutoModel.from_pretrained('monologg/koelectra-base-v3-discriminator')
+
+with st.form(key='my_form'):
+    text_input = st.text_input(label='Enter sentence')
+    submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+      with torch.no_grad():
+  
+        result = nlp(
+          text_input, 
+          model='monologg/koelectra-base-v3-discriminator',
+          tokenizer='monologg/koelectra-base-v3-discriminator')
+              
+        st.write(result)
